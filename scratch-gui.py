8b3c7c42fa1b25158c4925dbfbf6e00675e7a521
Sigton <sigton.scratch@gmail.com>
2016-08-26 15:42:43 +0100

    Added container and things

--- scratch-gui.py
+++ scratch-gui.py
@@ -30,3 +30,25 @@ class ScratchGUIApp(tk.Tk):
 
         # Set the window size
         self.geometry('{}x{}'.format(800,600))
+
+        # Create the container
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+
+        # And configure the grid
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        # Create a dictionary of frames and append all pages to it
+        self.frames = {}
+
+        for f in (LoginPage, MainPage):
+
+            frame = f(container, self)
+
+            self.frames[f] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        # Set the starting page
+        self.show_frame(LoginPage)
