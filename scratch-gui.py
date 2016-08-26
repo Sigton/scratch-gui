9181118b17aa903ca26a2112e583c7d9fc022dff
Sigton <sigton.scratch@gmail.com>
2016-08-26 15:39:15 +0100

    Started backend class

--- scratch-gui.py
+++ scratch-gui.py
@@ -7,3 +7,26 @@ This is a GUI built on Dylan5797's Scratch API
 
 import tkinter as tk
 from tkinter import ttk
+
+import scratchapi # Thanks to Dylan5797!
+
+LARGE_FONT = ("Verdana", 12)
+
+class ScratchGUIApp(tk.Tk):
+
+    '''
+    Main backend class, this is what makes stuff work.
+    '''
+
+    def __init__(self, *args, **kwargs):
+
+        ''' Constructor '''
+
+        # Call the parents constructor
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        # Set the window title
+        tk.Tk.wm_title(self, "Scratch GUI")
+
+        # Set the window size
+        self.geometry('{}x{}'.format(800,600))
